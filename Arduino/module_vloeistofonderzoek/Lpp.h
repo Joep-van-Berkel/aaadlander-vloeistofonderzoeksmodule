a09ebefa352b3e2a8aeaf5cf1c90b3cd17652a06
Joep van Berkel <150429156+Joep-van-Berkel@users.noreply.github.com>
2025-05-15 16:09:41 +0200

    library's and nRF24 setup (#62)

---
+++ Arduino/module_vloeistofonderzoek/Lpp.h
@@ -0,0 +1,27 @@
+#define LPP_DIGITAL_INPUT 0 // 1 byte
+#define LPP_DIGITAL_OUTPUT 1 // 1 byte
+#define LPP_ANALOG_INPUT 2 // 2 bytes, 0.01 signed
+#define LPP_ANALOG_OUTPUT 3 // 2 bytes, 0.01 signed
+#define LPP_GENERIC_SENSOR 100 // 4 bytes, unsigned
+#define LPP_LUMINOSITY 101 // 2 bytes, 1 lux unsigned
+#define LPP_PRESENCE 102 // 1 byte, bool
+#define LPP_TEMPERATURE 103 // 2 bytes, 0.1°C signed
+#define LPP_RELATIVE_HUMIDITY 104 // 1 byte, 0.5% unsigned
+#define LPP_ACCELEROMETER 113 // 2 bytes per axis, 0.001G
+#define LPP_BAROMETRIC_PRESSURE 115 // 2 bytes 0.1hPa unsigned
+#define LPP_VOLTAGE 116 // 2 bytes 0.01V unsigned
+#define LPP_CURRENT 117 // 2 bytes 0.001A unsigned
+#define LPP_FREQUENCY 118 // 4 bytes 1Hz unsigned
+#define LPP_PERCENTAGE 120 // 1 byte 1-100% unsigned
+#define LPP_ALTITUDE 121 // 2 byte 1m signed
+#define LPP_CONCENTRATION 125 // 2 bytes, 1 ppm unsigned
+#define LPP_POWER 128 // 2 byte, 1W, unsigned
+#define LPP_DISTANCE 130 // 4 byte, 0.001m, unsigned
+#define LPP_ENERGY 131 // 4 byte, 0.001kWh, unsigned
+#define LPP_DIRECTION 132 // 2 bytes, 1deg, unsigned
+#define LPP_UNIXTIME 133 // 4 bytes, unsigned
+#define LPP_GYROMETER 134 // 2 bytes per axis, 0.01 °/s
+#define LPP_COLOUR 135 // 1 byte per RGB Color
+#define LPP_GPS 136 // 3 byte lon/lat 0.0001 °, 3 bytes alt 0.01 meter
+#define LPP_SWITCH 142 // 1 byte, 0/1
+#define LPP_POLYLINE 240 // 1 byte size, 1 byte delta factor, 3 byte lon/lat 0.0001° * factor, n (size-8) bytes deltas
--- Arduino/module_vloeistofonderzoek/module_vloeistofonderzoek.ino
+++ Arduino/module_vloeistofonderzoek/module_vloeistofonderzoek.ino
@@ -1,133 +1,144 @@
-#include <RF24.h>
-#include <RF24_config.h>
-#include <nRF24L01.h>
-#include <printf.h>
-
+#include "lpp.h"
 #include "Led.h"
 #include <Arduino.h>
-
+#include <printf.h>
 #include <SPI.h>
-#include <nRF24L01.h>   // to handle this particular modem driver
-#include "RF24.h"       // the library which helps us to control the radio modem
+#include <nRF24L01.h>       // to handle this particular modem drive
+#include "RF24.h"           // the library which helps us to control the radio mode
 
-#define LEDPIN 3        // Ditital pin connected to the LED.
+#define LEDPIN 3            // the library which helps us to control the radio mode
+#define NOACK_PIN 4         // Digital pin to flag NOACK received by nRF2
+#define CE_PIN 7            // RF-NANO usb-c, Arduino-uno -> 7, RF-NANO micro-usb -> 9
+#define CSN_PIN 8           // RF-NANO usb-c, Arduino-uno -> 8, RF-NANO micro-usb -> 1
 
-// Initialise Sensors
+// Initialise sensors
 
-// Initialise Actuators
+// Initialise actuators
 Led led;
-int ledState = LOW;			              // ledState used to set the LED
+int ledState = LOW;         // ledState used to set the LED
+Led nRF24Led;
 
 #define RF24_PAYLOAD_SIZE 32
-#define AAAD_ARO 0
-#define AAAD_MODULE 2
+#define AAAD_ARO 2
+#define AAAD_MODULE 3
 
-/* Hardware configuration: Set up nRF24L01 radio on SPI bus plus pins 9 & 10, which are CE & CSN pins  */
-RF24 radio(9, 10);
-const uint8_t rf24_channel[] = {1,26,51,76,101}; // Radio channels set depending on satellite number
-const uint64_t addresses[] = { 0x4141414430LL, 0x4141414431LL, 0x4141414432LL, 0x4141414433LL, 0x4141414434LL };  //with radioNumber set to zero, the tx pipe will be 'AAAD0', which is basically HEX'4141414430', which is remote DESTINATION address for our transmitted data. The rx pipe code is the local receive address, which is what the remote device needs to set for the remote devices 'tx' pipe code.
+RF24 radio(CE_PIN, CSN_PIN);
+
+const uint8_t rf24_channel[] = {100,105,110,115,120}; 	             // Radio channels set depending on satellite number
+const uint64_t addresses[] = {
+  0x4141414430LL, 0x4141414431LL, 0x4141414432LL,
+  0x4141414433LL, 0x4141414434LL, 0x4141414435LL                     // with radioNumber set to zero, the tx pipe will be 'AAAD0', which is basically HEX'4141414430', which is remote DESTINATION address for our transmitted data. The rx pipe code is the local receive address, which is what the remote device needs to set for the remote devices 'tx' pipe code.
+};
+
 uint8_t txData[RF24_PAYLOAD_SIZE];
 uint8_t rxData[RF24_PAYLOAD_SIZE];
+uint8_t bytes;
 
 // Timing configuration
-unsigned long previousMillis = 0;     // will store last time LED was updated
+unsigned long previousMillis = 0;                                     // will store last time LED was update
 unsigned long currentMillis;
-unsigned long sampleTime = 1000;   // milliseconds of on-time
+unsigned long sampleTime = 5000;                                      // milliseconds of on-tim
 
 // int to hex converter
-void printHex2(unsigned v) {
-    Serial.print("0123456789ABCDEF"[v>>4]);
-    Serial.print("0123456789ABCDEF"[v&0xF]);
+void print2Hex(unsigned v) {
+  Serial.print("0123456789ABCDEF"[v >> 4]);
+  Serial.print("0123456789ABCDEF"[v & 0xF]);
 }
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("nRF24 Application ARO" + String(AAAD_ARO) + ", Module" + String(AAAD_MODULE) + " Started!\n");
-
-
-  // Activate sensors
+  Serial.println("\n\nnRF24 Application ARO" + String(AAAD_ARO) + ", Module" + String(AAAD_MODULE) + " Started!\n");
 
   // Activate actuators
   led.begin(LEDPIN);
   led.setState(ledState);
+  nRF24Led.begin(NOACK_PIN);
+  nRF24Led.setState(LOW);
 
   // Activate Radio
-  radio.begin();                  // Ativate the modem
-  radio.setPALevel(RF24_PA_MIN);  // Set the PA Level low to prevent power supply related issues
-                                  // since this is a getting_started sketch, and the likelihood of close proximity of the devices. RF24_PA_MAX is default.
-  radio.setDataRate(RF24_1MBPS);  // choosing 1 Mega bit per second radio frequency data rate
-                                  // radio frequency data rate choices are:  //RF24_250KBPS    //RF24_2MBPS  //RF24_1MBPS
+  printf_begin();
+  SPI.begin();
+  radio.begin();                                                     // Ativate the modem
+  radio.setAddressWidth(5);                                          // Set Address width
+  radio.setRetries(15, 15);                                          // delaytime 4000uS and retry count is 1
+  radio.setPayloadSize(RF24_PAYLOAD_SIZE);    
+  radio.setPALevel(RF24_PA_HIGH, 1);                                 // Set the PA Level low to prevent power supply related issues
+  radio.setDataRate(RF24_250KBPS);                                   // choosing 250K bit per second radio frequency data rate
   radio.setChannel(rf24_channel[AAAD_ARO]);
-  radio.setPayloadSize(RF24_PAYLOAD_SIZE);
   radio.openWritingPipe(addresses[AAAD_MODULE]);
   radio.openReadingPipe(1, addresses[AAAD_MODULE]);
 
-  // Start the radio listening for data
+  Serial.println("\nnRF24 Setup Initialized");
+  radio.printDetails();
+  delay(500);
   radio.startListening();
 }
 
 void loop() {
-  // check to see if it's time to change the state of the LED
-  currentMillis = millis();
+  currentMillis = millis();                                          // check to see if it's time to change the state of the LE
 
-  if(currentMillis - previousMillis >= sampleTime) {
-   
-    unsigned long timeStamp = millis()/1000;
+  if (currentMillis - previousMillis >= sampleTime) {
+    unsigned long timeStamp = millis() / 1000;
     uint8_t cursor = 0;
+    txData[cursor++] = 1;                                            // channel number
+    txData[cursor++] = LPP_UNIXTIME;                                 // Datatype LPP_UNIXTIM
     txData[cursor++] = timeStamp >> 24;
     txData[cursor++] = timeStamp >> 16;
     txData[cursor++] = timeStamp >> 8;
     txData[cursor++] = timeStamp;
-    while (cursor<RF24_PAYLOAD_SIZE) {
-      txData[cursor++] = 0;
-    }
-        
-  /****************** Transmit Mode ***************************/
+    while (cursor < RF24_PAYLOAD_SIZE) {txData[cursor++] = 0;}       // Fill up the array with zero
 
-   // Print transmit data in Hex format
+/****************** Transmit Mode ***************************/
+
+    // Print transmit data in Hex format
     Serial.print("txData: ");
-    for (size_t i=0; i<cursor; ++i) {
+    for (size_t i = 0; i < cursor; ++i) {
       if (i != 0) Serial.print(" ");
-      printHex2(txData[i]);
+      print2Hex(txData[i]);
     }
     Serial.println();
 
-    radio.stopListening();  // First, stop listening so we can talk.
-    // Serial.println(F("Now Sending"));
+    radio.stopListening();                                            // First, stop listening so we can talk
 
     // Transmit data to radio
-    radio.write(&txData, sizeof(txData));
-
-    radio.startListening();  // Now, continue listening
-    // Serial.println(F("Now Listing"));
-
+    if (radio.write(&txData, sizeof(txData))) {
+      Serial.println("ACK received!");
+      nRF24Led.setState(LOW);                                         // Clear led to signal there is communicatio
+    } else {
+      Serial.println("No ACK received!");
+      nRF24Led.setState(HIGH);                                        // Set led to signal no communicatio
+    }
+    radio.startListening();                                           // Now, continue listenin
     previousMillis = currentMillis;
   }
 
   /****************** Receive Mode ***************************/
 
-  if (radio.available()) {      //'available' means whether valid bytes have been received and are waiting to be read from the receive buffer
-    // Receive data from radio
-    while (radio.available()) { // While there is data ready
-      radio.read(&rxData, sizeof(rxData));  // Get the payload
+  if (radio.available()) {                                            //'available' means whether valid bytes have been received and are waiting to be read from the receive buffe
+    while (radio.available()) {                                       // While there is data read
+      bytes = radio.getPayloadSize();
+      radio.read(rxData, bytes);                                      // read value from the configured pip
+      radio.read(&rxData, sizeof(rxData));                            // Get the payload
     }
+
     // Print received data in Hex format
     Serial.print("rxData: ");
-    for (size_t i=0; i<RF24_PAYLOAD_SIZE; ++i) {
+    for (size_t i = 0; i < RF24_PAYLOAD_SIZE; ++i) {
       if (i != 0) Serial.print(" ");
-      printHex2(rxData[i]);
+      print2Hex(rxData[i]);
     }
     Serial.println();
 
     // Switch led on Received command
-    if (rxData[0]==0xFF) {
-      Serial.println("Led=on");
-      led.setState(HIGH);
-    }
-    if (rxData[0]==0x7F) {
-      Serial.println("Led=off");
-      led.setState(LOW);
+    if (rxData[0] == 0 && rxData[1] == LPP_DIGITAL_OUTPUT) {           // channelnumber == 0 and Datatype is LPP_DIGITAL_OUTPUT
+      if (rxData[2] == 0xFF) {
+        Serial.println("Led=ON");
+        led.setState(HIGH);
+      }
+      if (rxData[2] == 0x7F) {
+        Serial.println("Led=OFF");
+        led.setState(LOW);
+      }
     }
   }
-
-}  // Loop
+}   // Loop
